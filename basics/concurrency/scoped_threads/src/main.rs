e9c1884a158f983a413929d7cff9c7a30ae34528
Vyacheslav Tilikov <vyutilikov@avito.ru>
2025-10-09 00:49:25 +0300

    threads: scoped threads

--- basics/concurrency/scoped_threads/src/main.rs
+++ basics/concurrency/scoped_threads/src/main.rs
@@ -1,3 +1,19 @@
+const N_THREADS: usize = 8;
+
 fn main() {
-    println!("Hello, world!");
+    let to_add = (0..5000).collect::<Vec<u32>>();
+    let chunks = to_add.chunks(N_THREADS);
+
+    let sum = std::thread::scope(|s| {
+        let mut handlers = Vec::new();
+
+        for chunk in chunks {
+            let handler = s.spawn(move || chunk.iter().sum::<u32>());
+            handlers.push(handler);
+        }
+
+        handlers.into_iter().map(|h| h.join().unwrap()).sum::<u32>()
+    });
+
+    println!("Sum: {:?}", sum);
 }
